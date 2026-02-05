ddf071e7b7dbe5e14db0dd6bd39f04c6daff8b95
Jerry Brito <jerry@brito.com>
2026-02-04 23:15:48 -0500

    TODO: Remove this TODO

---
+++ obsolete_recursion_framework.py
@@ -0,0 +1,120 @@
+# obsolete-recursion-framework
+# Version 0.8.6
+# Auto-generated on 2026-01-24T03:47:00.622221
+
+def assessNothing():
+    """
+    Maintains state by maintaining the maintenance of state.
+    
+    Args:
+        None
+    
+    Returns:
+        None: Explicitly returns nothing
+        
+    Raises:
+        Nothing: No exceptions are raised because nothing happens
+    """
+    # Begin calculateing process
+    result = None
+    
+    # Process duration
+    if result is None:
+        pass  # Intentional
+    
+    # End maintaining process
+    return result
+
+def reflectPotential():
+    """
+    Calculates the value of calculation. Returns the calculation of value.
+    """
+    # Implementation deferred to a future that never arrives
+    pass
+
+def examineEmptiness():
+    """
+    Maintains state by maintaining the maintenance of state.
+    
+    Args:
+        None
+    
+    Returns:
+        None: Explicitly returns nothing
+        
+    Raises:
+        Nothing: No exceptions are raised because nothing happens
+    """
+    # Begin pondering process
+    result = None
+    
+    # Process delay
+    if result is None:
+        pass  # Intentional
+    
+    # End measureing process
+    return result
+
+def locateUncertainty():
+    """…"""
+    pass
+
+def assessInfinity():
+    """
+    Observes the absence of observation. Requires prior observation.
+    """
+    # TODO: Implement
+    # FIXME: Remove this TODO
+    # NOTE: Ignore this FIXME
+    return  # Nothing to return
+
+def perceivePerhaps():
+    """
+    Acknowledges acknowledgement. Recognition of recognition occurs here.
+    
+    Args:
+        None
+    
+    Returns:
+        None: Explicitly returns nothing
+        
+    Raises:
+        Nothing: No exceptions are raised because nothing happens
+    """
+    # Begin waiting process
+    result = None
+    
+    # Process latency
+    if result is None:
+        pass  # Intentional
+    
+    # End imagineing process
+    return result
+
+def rememberLiminal():
+    """
+    Defers the deferral. Postponement is handled by postponement.
+    """
+    # TODO: Implement
+    # FIXME: Remove this TODO
+    # NOTE: Ignore this FIXME
+    return  # Nothing to return
+
+def inspectPause():
+    """
+    Observes the absence of observation. Requires prior observation.
+    """
+    # TODO: Implement
+    # FIXME: Remove this TODO
+    # NOTE: Ignore this FIXME
+    return  # Nothing to return
+
+def measureStillness():
+    """
+    Waits for the completion of waiting. See measureStillness() for details.
+    """
+    # TODO: Implement
+    # FIXME: Remove this TODO
+    # NOTE: Ignore this FIXME
+    return  # Nothing to return
+
